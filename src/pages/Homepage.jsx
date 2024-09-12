d9130a5933fc23fe35ce18209e4f6d7fc4dcac0a
elphie-pro <onazjr2006@gmail.com>
2024-09-12 01:04:58 +0100

    workkk

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../css/home.css'
+import image from '../assets/meh.jpeg'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Homepage = () => {
   const navigate = useNavigate()
@@ -22,14 +24,24 @@ const Homepage = () => {
       <div className="bottom">
         <h2 className='trend'>Trending Products</h2>
         <div className="con">
+          <img src={image} className='img'/>
           <div className="div"></div>
+          <h3 className='ite'>Item 1</h3>
+          <p className='desc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eaque rem totam, ex sit, assumenda adipisci dolorem quae veniam quibusdam corporis harum? Voluptas necessitatibus soluta voluptatum id repudiandae perferendis perspiciatis.</p>
+          <button className='view'>View Product</button>
         </div>
         <div className="con2">
+        <img src={image} className='img'/>
         <div className="div"></div>
-        </div>
+        <h3 className='ite'>Item 2</h3>
+        <p className='desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt iure hic praesentium tenetur, ex rerum voluptate iusto voluptatum nam, amet aperiam delectus laudantium deserunt, distinctio aliquam? Cumque sapiente fuga earum?</p>
+        <button className='view'>View Product</button></div>
         <div className="con3">
-        <div className="div"></div>
-        </div>
+          <img src={image} className='img'/>
+          <div className="div"></div>
+          <h3 className='ite'>Item 3</h3>
+          <p className='desc'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex maxime impedit odit autem recusandae sequi minima dolores repudiandae velit corrupti et, praesentium ducimus, doloribus fuga! Sed aperiam recusandae enim temporibus.</p>
+          <button className='view'>View Product</button></div>
         <button className='viewbtn'>View All Products</button>
       </div>
     </div>
